e4ca477b463b0fe35955636e6eaf1676efd4f9b1
Rizart Dokollari <r.dokollari@gmail.com>
2021-10-23 00:56:50 +0100

    Assert handles failing and passing rule

--- 02_a_first_unit_test/password-verifier.test.js
+++ 02_a_first_unit_test/password-verifier.test.js
@@ -5,7 +5,6 @@ describe(`${PasswordVerifier.name}`, () => {
     describe("with a failing rule", () => {
       const passwordVerifier = new PasswordVerifier();
       const fakeRule = () => ({ passed: false, reason: "fake reason" });
-
       passwordVerifier.addRule(fakeRule);
       const errors = passwordVerifier.verify("failing rule", [fakeRule]);
 
@@ -14,6 +13,35 @@ describe(`${PasswordVerifier.name}`, () => {
       });
 
       it("has exactly one error", () => {
+        expect(errors.length).toBe(1);
+      });
+    });
+    describe("with a passing rule", () => {
+      const passwordVerifier = new PasswordVerifier();
+      const fakeRule = () => ({ passed: true, reason: "fake reason" });
+      passwordVerifier.addRule(fakeRule);
+      const errors = passwordVerifier.verify("passing rule", [fakeRule]);
+
+      it("has no errors", () => {
+        expect(errors.length).toBe(0);
+      });
+    });
+
+    describe("with a failing and a passing rule", () => {
+      const passwordVerifier = new PasswordVerifier();
+      const fakeRulePass = () => ({ passed: true, reason: "fake reason" });
+      const fakeRuleFail = () => ({ passed: false, reason: "fake reason" });
+      passwordVerifier.addRule(fakeRulePass);
+      passwordVerifier.addRule(fakeRuleFail);
+      const errors = passwordVerifier.verify("passing rule", [
+        fakeRulePass,
+        fakeRuleFail,
+      ]);
+
+      it("error text belongs to failed rule", () => {
+        expect(errors[0]).toContain("fake reason");
+      });
+      it("has one error", () => {
         expect(errors.length).toBe(1);
       });
     });
