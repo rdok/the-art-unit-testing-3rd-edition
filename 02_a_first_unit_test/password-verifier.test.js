b266c423286016b4820864180b9cef75a2ac4561
Rizart Dokollari <r.dokollari@gmail.com>
2021-10-25 07:17:59 +0100

    Use factories on each test

--- 02_a_first_unit_test/password-verifier.test.js
+++ 02_a_first_unit_test/password-verifier.test.js
@@ -1,51 +1,50 @@
 const { PasswordVerifier } = require("./password-verifier");
-const { makeFailingRule, makePassingRule } = require("../jest/makers");
+const {
+  makeVerifierWithFailingRule,
+  makerVerifierWithPassingRule,
+  makeVerifierWithFailingAndPassingRule,
+} = require("../jest/password-verifier-factory");
 
-describe(`${PasswordVerifier.name}`, () => {
-  describe(`${PasswordVerifier.prototype.verify.name}`, () => {
-    describe("with a failing rule", () => {
-      const passwordVerifier = new PasswordVerifier();
-      passwordVerifier.addRule(makeFailingRule("fake reason"));
+describe(`${PasswordVerifier.name}.${PasswordVerifier.prototype.verify.name}`, () => {
+  describe("with a failing rule", () => {
+    it("has an error message based on the rule reason", () => {
+      const passwordVerifier = makeVerifierWithFailingRule();
       const errors = passwordVerifier.verify("failing rule");
-
-      it("has an error message based on the rule reason", () => {
-        expect(errors[0]).toContain("fake reason");
-      });
-
-      it("has exactly one error", () => {
-        expect(errors.length).toBe(1);
-      });
-    });
-    describe("with a passing rule", () => {
-      const passwordVerifier = new PasswordVerifier();
-      passwordVerifier.addRule(makePassingRule());
-      const errors = passwordVerifier.verify("passing rule");
-
-      it("has no errors", () => {
-        expect(errors.length).toBe(0);
-      });
+      expect(errors[0]).toContain("fake reason");
     });
 
-    describe("with a failing and a passing rule", () => {
-      const passwordVerifier = new PasswordVerifier();
-      passwordVerifier.addRule(makePassingRule());
-      passwordVerifier.addRule(makeFailingRule("fake reason"));
+    it("has exactly one error", () => {
+      const passwordVerifier = makeVerifierWithFailingRule();
+      const errors = passwordVerifier.verify("failing rule");
+      expect(errors.length).toBe(1);
+    });
+  });
+  describe("with a passing rule", () => {
+    it("has no errors", () => {
+      const passwordVerifier = makerVerifierWithPassingRule();
       const errors = passwordVerifier.verify("passing rule");
-
-      it("error text belongs to failed rule", () => {
-        expect(errors[0]).toContain("fake reason");
-      });
-      it("has one error", () => {
-        expect(errors.length).toBe(1);
-      });
+      expect(errors.length).toBe(0);
     });
   });
 
-  describe(`${PasswordVerifier.prototype.addRule.name}`, () => {
-    it("adds a rule", () => {
-      const passwordVerifier = new PasswordVerifier();
-      passwordVerifier.addRule("a rule");
-      expect(passwordVerifier.rules).toEqual(["a rule"]);
+  describe("with a failing and a passing rule", () => {
+    it("error text belongs to failed rule", () => {
+      const passwordVerifier = makeVerifierWithFailingAndPassingRule();
+      const errors = passwordVerifier.verify("passing rule");
+      expect(errors[0]).toContain("fake reason");
     });
+    it("has one error", () => {
+      const passwordVerifier = makeVerifierWithFailingAndPassingRule();
+      const errors = passwordVerifier.verify("passing rule");
+      expect(errors.length).toBe(1);
+    });
+  });
+});
+
+describe(`${PasswordVerifier.name}.${PasswordVerifier.prototype.addRule.name}`, () => {
+  it("adds a rule", () => {
+    const passwordVerifier = new PasswordVerifier();
+    passwordVerifier.addRule("a rule");
+    expect(passwordVerifier.rules).toEqual(["a rule"]);
   });
 });
